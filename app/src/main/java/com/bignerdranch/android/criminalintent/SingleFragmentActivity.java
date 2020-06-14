2ccf90f53a13accf6cb736dd8d06935f8c1bdc52
Kevin Liu <yuhliu@umich.edu>
2020-06-14 02:37:17 -0400

    Chapter 8 Complete

--- app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -1,23 +1,26 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import android.os.Bundle;
+public abstract class SingleFragmentActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_fragment);
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
         if (fragment == null) {
-            fragment = new CrimeFragment();
+            fragment = createFragment();
             fm.beginTransaction()
                     .add(R.id.fragment_container, fragment)
                     .commit();
